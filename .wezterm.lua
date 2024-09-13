801cd85f79c4eefcd18b0354d62302278e6a0dbe
doubleun <nitsomboon77@gmail.com>
2024-09-13 10:06:38 +0700

    add options and cmd arrow keys for wezTerm

--- .wezterm.lua
+++ .wezterm.lua
@@ -25,6 +25,20 @@ config.keys = {
 		mods = "CMD",
 		action = act.ClearScrollback("ScrollbackAndViewport"),
 	},
+	-- Make Option-Left equivalent to Alt-b which many line editors interpret as backward-word
+	{ key = "LeftArrow", mods = "OPT", action = wezterm.action({ SendString = "\x1bb" }) },
+	-- Make Option-Right equivalent to Alt-f; forward-word
+	{ key = "RightArrow", mods = "OPT", action = wezterm.action({ SendString = "\x1bf" }) },
+	{
+		key = "LeftArrow",
+		mods = "CMD",
+		action = wezterm.action({ SendString = "\x1bOH" }),
+	},
+	{
+		key = "RightArrow",
+		mods = "CMD",
+		action = wezterm.action({ SendString = "\x1bOF" }),
+	},
 }
 
 return config
